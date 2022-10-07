e4a3f202d029241c09bdce175e8aae20d95f51c4
Rizki Irfan Anshori <ilfan.indo@gmail.com>
2022-10-07 09:45:14 +0700

    Update index.php

--- serverside/get-page/index.php
+++ serverside/get-page/index.php
@@ -1,7 +1,9 @@
 <?php
+    //mengambil konfigurasi
+    require $_SERVER['DOCUMENT_ROOT']."/serverside/config.php"; 
     if (!empty($_GET['site'])){
         if (!filter_var($_GET['site'], FILTER_VALIDATE_URL) === false) {
-            echo file_get_contents(filter_var($_GET['site'], FILTER_SANITIZE_URL));
+            echo getUrl(filter_var($_GET['site'], FILTER_SANITIZE_URL));
         }
     }
     die();
