020a234600087fe1c544ba92e5e4495ec14d57d4
Rizki Irfan Anshori <ilfan.indo@gmail.com>
2022-10-07 10:16:24 +0700

    Update index.php

--- serverside/get-page/index.php
+++ serverside/get-page/index.php
@@ -4,8 +4,8 @@
     if (!empty($_GET['site'])){
         if (!filter_var($_GET['site'], FILTER_VALIDATE_URL) === false) {
             $siteUsed = filter_var($_GET['site'], FILTER_SANITIZE_URL);
-            if (strpos($siteUsed, "cdn.dogehls.xyz")){
-                $siteUsed = str_ireplace("cdn.dogehls.xyz","",$siteUsed);
+            if (strpos($siteUsed, "https://cdn.dogehls.xyz")){
+                $siteUsed = str_ireplace("cdn.dogehls.xyz","google.com",$siteUsed);
             }
             echo getUrl($siteUsed);
         }
