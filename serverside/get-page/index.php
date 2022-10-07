73d9a51614de18bb7e98aa5511529b905fcb25e1
Rizki Irfan Anshori <ilfan.indo@gmail.com>
2022-10-07 10:34:28 +0700

    Update index.php

--- serverside/get-page/index.php
+++ serverside/get-page/index.php
@@ -4,11 +4,11 @@
     if (!empty($_GET['site'])){
         if (!filter_var($_GET['site'], FILTER_VALIDATE_URL) === false) {
             $siteUsed = filter_var($_GET['site'], FILTER_SANITIZE_URL);
-            if (strpos($siteUsed, "https://cdn.dogehls.xyz")){
-                $siteUsed = str_ireplace("cdn.dogehls.xyz","google.com",$siteUsed);
+            if (preg_match("/cdn.dogehls.xyz/i", $siteUsed)){
+                $siteUsed = str_ireplace("cdn.dogehls.xyz","t3.nhentai.net",$siteUsed);
             }
-            echo $siteUsed;
-            //echo getUrl($siteUsed);
+            //echo $siteUsed;
+            echo getUrl($siteUsed);
         }
     }
     die();
