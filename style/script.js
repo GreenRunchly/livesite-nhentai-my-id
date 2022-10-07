5fcc3f3fe6528c462c36974dae8892da00bc0fb6
Rizki Irfan Anshori <ilfan.indo@gmail.com>
2022-10-07 09:20:30 +0700

    Update script.js

--- style/script.js
+++ style/script.js
@@ -1,6 +1,6 @@
 /* Variable */
-var appLink = 'https://nhentai.local';
-var appLinkServer = 'https://nhentai.local/serverside/backdoor';
+var appLink = 'https://nhentai.my.id';
+var appLinkServer = 'https://nhentai.my.id/serverside/backdoor';
 var appPremium = true;
 /* System Functions */
 function setData(input,pada){
