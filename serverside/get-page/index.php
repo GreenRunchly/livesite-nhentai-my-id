70ae83cbb523d4f48ce0a59ff881cf1d9c23da8b
Rizki Irfan Anshori <36106812+GreenRunchly@users.noreply.github.com>
2022-12-20 22:00:37 +0700

    Update index.php
    Signed-off-by: Rizki Irfan Anshori <36106812+GreenRunchly@users.noreply.github.com>

--- serverside/get-page/index.php
+++ serverside/get-page/index.php
@@ -1,6 +1,6 @@
 <?php
     //mengambil konfigurasi
-    require $_SERVER['DOCUMENT_ROOT']."/serverside/config.php"; 
+    require $_SERVER['DOCUMENT_ROOT']."/nh/serverside/config.php"; 
     if (!empty($_GET['site'])){
         if (!filter_var($_GET['site'], FILTER_VALIDATE_URL) === false) {
             $siteUsed = filter_var($_GET['site'], FILTER_SANITIZE_URL);
