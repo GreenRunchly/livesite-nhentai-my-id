db4e72872e921b4869f2e3fedd7ba130ec10441f
Rizki Irfan Anshori <ilfan.indo@gmail.com>
2022-12-20 22:26:02 +0700

    Create debug-img.php

---
+++ serverside/backdoor/debug-img.php
@@ -0,0 +1,17 @@
+<?php
+	header('Content-Type: image/jpg; charset=utf-8');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET');
+    header("Access-Control-Allow-Headers: X-Requested-With");
+    session_start();
+
+    //mengambil konfigurasi
+    require $_SERVER['DOCUMENT_ROOT']."/nh/serverside/config.php";
+	
+    require_once(DIR_SITUS.'/simple_html_dom.php');
+
+        //$html = getUrl(URL_SITUS_MIDDLE_MAN.URL_SITUS_CORS.'/g/364599');
+        //https://cdn.dogehls.xyz/galleries/1947769/cover.jpg
+        $html = getUrl('https://cdn.dogehls.xyz/galleries/1947769/cover.jpg');
+	echo $html;
+?>
